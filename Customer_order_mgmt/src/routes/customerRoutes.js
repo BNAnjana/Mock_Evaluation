55689095f6482c1dce4208416078f03433f9cd16
BNAnjana <nagaanjana003@gmail.com>
2026-01-25 19:35:59 +0530

    second commit

---
+++ Customer_order_mgmt/src/routes/customerRoutes.js
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import { registerCustomer, deleteCustomer } from '../controllers/customerController.js';
+
+const router = Router();
+
+router.post('/register', registerCustomer);
+router.delete('/delete/:customerId', deleteCustomer);
+
+export default router;
